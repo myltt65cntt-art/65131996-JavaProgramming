93fd0318fd02804d5873a9f1450126e602ada20f
myltt65cntt-art <my.ltt.65cntt@ntu.edu.vn>
2026-01-26 16:07:56 +0700

    TH1: HelloWorld

---
+++ TH1/src/com/a65131996/th1/HelloWorldTextblocks.java
@@ -0,0 +1,14 @@
+package com.a65131996.th1;
+public class HelloWorldTextblocks {
+    public static void main(String[] args) {
+        // Text blocks với """
+        String greeting = """
+            ======================
+            HELLO, MODERN JAVA!
+            ======================
+            by Mai Cường Thọ NTU
+            ======================
+            """;
+        System.out.println(greeting);
+    }
+}
---
+++ TH1/src/com/a65131996/th1/Hinhve.java
@@ -0,0 +1,10 @@
+package com.a65131996.th1;
+
+public class Hinhve {
+    public static void main(String[] args) {
+        System.out.println("   *   ");
+        System.out.println("  ***  ");
+        System.out.println(" ***** ");
+        System.out.println("*******");
+    }
+}
