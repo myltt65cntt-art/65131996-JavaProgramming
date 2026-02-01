5f15a868f95f0cb18df72e8da103082751b95d1b
myltt65cntt-art <my.ltt.65cntt@ntu.edu.vn>
2026-02-01 13:32:16 +0700

    TH1: HelloWorld

---
+++ TH1/src/com/a65131996/th1/BaiTap1.java
@@ -0,0 +1,16 @@
+package com.a65131996.th1;
+
+public class BaiTap1 {
+    public static void main(String[] args) {
+        // 1. Khai báo các biến
+        int a = 31;
+        int b = 2;
+        double c = 2;
+        int tong = a + b;
+        double thuong = a / c;
+        int phanDu = a % b;
+        System.out.println("Tổng: " + tong);
+        System.out.println("Thương: " + thuong);
+        System.out.println("Phần dư: " + phanDu);
+    }
+}
